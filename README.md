2cadfa87581545394ecc3f9de2b4058942db3ef0
dohako <dohako@inbox.ru>
2021-03-31 15:25:24 +0300

    DEV/CHG dev ver1, changed readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# WebAPI_test
+# WebAPI test
+
+### v1
+Works awful, but works. Response time is about 7 seconds.
+API only on Flask
+
+### Setup and Start
+* `git clone...`
+* `pip install -r requirements.txt`
+* `python flask_web_api.py`
+* get csv from https://disk.yandex.ru/d/npzjVSfFyH7Cww
+
+### Dependencies
+* python 3.8 and higher;
+* git
+* modules: flask, flask_restful
+* recommends.csv
--- flask_web_api.py
+++ flask_web_api.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+import data
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,9 +9,18 @@ api = Api(app)
 class Product(Resource):
     """
     need to have only one endpoint that will takes to args
+    TestMe: http://127.0.0.1:5000/product?sku=WP260qJAo6&rank=0.9
     """
     def get(self):
-        return "Hello", 200
+        parser = reqparse.RequestParser()
+        parser.add_argument("sku",required=True)
+        parser.add_argument("rank",required=False)
+
+        args = parser.parse_args()
+
+        result = data.get_rec_sku_csv(args['sku'],args['rank'])
+
+        return result, 200
 
 
 api.add_resource(Product,'/product')
